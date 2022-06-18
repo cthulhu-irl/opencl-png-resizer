269b49c43d4bac3c656c48af0cb873883709a703
cthulhu <xh4x0r3r@gmail.com>
2022-06-19 01:10:19 +0430

    fix: use default device type

--- src/openclmanager.cpp
+++ src/openclmanager.cpp
@@ -4,24 +4,8 @@
 
 void OpenCLManager::create_context() {
     try {
-        std::vector<cl::Platform> platforms;
-        cl::Platform::get(&platforms);
-        if (platforms.empty())
-            throw;
-        platform_ = platforms[0];
-
-        std::vector<cl::Device> devices;
-        platform_.getDevices(CL_DEVICE_TYPE_GPU, &devices);
-
-        if (devices.empty())
-            platform_.getDevices(CL_DEVICE_TYPE_CPU, &devices);
-
-        if (devices.empty())
-            throw;
-
-        device_ = devices[0];
-        context_ = cl::Context({device_});
-        queue_ = cl::CommandQueue(context_, device_);
+        context_ = cl::Context(CL_DEVICE_TYPE_DEFAULT);
+        queue_ = cl::CommandQueue(context_);
     } catch(...) {
         throw std::runtime_error("Failed to create an OpenCL context!");
     }
