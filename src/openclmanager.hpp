0c0fcca2a018f23e4d46969705fb16651ed206b9
cthulhu <xh4x0r3r@gmail.com>
2022-06-19 03:34:11 +0430

    fix: use constructor instead of lateral assignment

--- src/openclmanager.hpp
+++ src/openclmanager.hpp
@@ -11,25 +11,30 @@
 // edited from: https://github.com/vencabkk/opencl-resizer/
 
 class OpenCLManager {
-    cl::Platform     platform_;
-    cl::Device       device_;
     cl::Context      context_;
     cl::CommandQueue queue_;
     cl::Program      program_;
 
   public:
-    OpenCLManager() { create_context(); }
+    OpenCLManager()
+        : context_(CL_DEVICE_TYPE_DEFAULT)
+        , queue_(context_)
+    {}
 
+    OpenCLManager(const char* program_src)
+        : context_(CL_DEVICE_TYPE_DEFAULT)
+        , queue_(context_)
+        , program_(context_, program_src, true)
+    {}
+
+    // TODO check `program_`'s validity.
     bool create_kernel_program(const char* program_src) {
-      program_ = cl::Program(context_, program_src, true);
-      // TODO check `program_`'s validity.
+      program_ = cl::Program(context_, program_src);
+      program_.build();
       return true;
     }
 
     cl::Program get_program() { return program_; }
     cl::Context get_context() { return context_; }
     cl::CommandQueue get_queue() { return queue_; }
-
-  private:
-    void create_context();
 };
